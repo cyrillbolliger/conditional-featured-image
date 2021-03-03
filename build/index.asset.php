aa4e02e60bac6fdec316aa69bacd0c73fc7e49c3
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-03-03 22:12:04 +0100

    updated dependencies

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => 'f59da54645bdb6f8c15363af059a0615');
+<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => '272f20259d87a26295bffc3e52e398ad');
