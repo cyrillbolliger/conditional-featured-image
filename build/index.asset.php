8d2ebb61336842e1490c8018ae5ab127140eb93d
Cyrill Bolliger <mail@cyrill.me>
2023-04-16 20:02:52 +0200

    [DEPS] Updated dependencies

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => '14b050e60b0e74c39d77368c8cb80d4e');
+<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => '98336e3b2295a82bbab3');
